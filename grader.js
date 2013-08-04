d54cf82de5256564df341f883e6ced0bc86e682f
startupjw <ubuntu@ip-172-31-26-190.us-west-2.compute.internal>
2013-08-04 22:00:17 +0000

    Updated index.html

--- grader.js
+++ grader.js
@@ -1,48 +1,108 @@
-/* Requires */
-var fs = require('fs');
-var program = require('./node_modules/commander.js');
-var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
 
-function assertFileExists(infile)
-{
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+var thing;
+var fs = require('fs');
+var flag = 0;
+var program = require('commander');
+var cheerio = require('cheerio');
+var resuult = '';
+var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "DEFAULT_URL, NO URL EXIST";
+var CHECKSFILE_DEFAULT = "checks.json";
+var restler = require('restler');
+var util = require('util');
+var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr))
-    {
+    if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(0);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
-}
-function cheerioHtmlFile(htmlfile)
+};
+function dumbfunction(result)
 {
+    thing = result;
+    resuult = checkHtmlFile(program.file, program.checks, program.url);
+    var outJson = JSON.stringify(resuult, null, 4);
+    console.log(outJson);
+}
+var assertURL = function(URL)
+{
+    return URL;
+};
+var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
-}
-function loadChecks(checksfile)
-{
+};
+
+var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
-}
-function checkHtmlFile(htmlfile, checksfile)
-{
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = new Array();
-    for(var ii in checks)
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, url) {
+    if(url != URL_DEFAULT)
     {
+        $ = cheerio.load(thing.toString());
+    }
+    if(url == URL_DEFAULT)
+    {
+        $ = cheerioHtmlFile(htmlfile);
+    }
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
-}
-if(require.main == module)
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+function checkHtmlFile1(file, checks, url)
 {
-    program.option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT).option('-f, --file ', 'Path to index.html' assertFileExists, HTMLFILE_DEFAULT).parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(url == URL_DEFAULT)
+    {
+        return checkHtmlFile(program.file, program.checks, program.url);
+    }
+    else
+    {
+        restler.request(url, "utf-8").on("complete", dumbfunction);
+    }
+}
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', clone(assertURL), URL_DEFAULT).parse(process.argv);
+    var checkJson = checkHtmlFile1(program.file, program.checks, program.url);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-}
-else
-{
+    if(program.url == URL_DEFAULT)
+    {
+        console.log(outJson);
+    }
+} else {
     exports.checkHtmlFile = checkHtmlFile;
 }
